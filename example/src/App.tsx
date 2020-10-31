96ea7e4b5bb2e902135d835b03f21a8b3826a4eb
ptrocki <you@example.com>
2020-10-31 19:34:37 +0100

    add release it

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import SvgView from 'react-native-svg-view';
 
 export default function App() {
